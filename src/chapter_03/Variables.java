17a2807ae91c9944afefa2c8c261657cbd32a111
Neil.Crerar <Neil.Crerar@bjss.com>
2019-07-22 12:02:33 +0100

    Additional Chptr 3 content

---
+++ src/chapter_03/Variables.java
@@ -0,0 +1,75 @@
+package Chapter03;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        /* TYPE: byte
+           MIN: -128
+           MAX: 127
+           DEFAULT: 0
+         */
+        byte myByteVariable = 23;
+        System.out.println("Byte variable: " + myByteVariable);
+
+        /* TYPE: short
+           MIN: -32,768
+           MAX: 32,767
+           DEFAULT: 0
+         */
+        short myShortVariable = 1234;
+        System.out.println("Short variable: " + myShortVariable);
+
+        /* TYPE: int
+           MIN: -2,147,483,648
+           MAX: 2,147,483,647
+           DEFAULT: 0
+         */
+        int myIntVariable = 543677;
+        System.out.println("Int variable: " + myIntVariable);
+
+        /* TYPE: long
+           MIN: -9,223.372.036.854.775.808
+           MAX: 9,223,372,036,854,775,807
+           DEFAULT: 0
+         */
+        long myLongVariable = 132231050566456L;
+        System.out.println("Long variable: " + myLongVariable);
+
+        /* TYPE: float
+           MIN:
+           MAX:
+           DEFAULT:
+         */
+        float myFloatVariable = 23.6f;
+        System.out.println("Float variable: " + myFloatVariable);
+
+        /* TYPE: double
+           MIN:
+           MAX:
+           DEFAULT:
+         */
+        double myDoubleVariable = 12.56;
+        System.out.println("Double variable: " + myDoubleVariable);
+
+        /* TYPE: boolean
+           MIN:
+           MAX:
+           DEFAULT:
+         */
+        boolean myBooleanVariable = false;
+        System.out.println("Boolean variable: " + myBooleanVariable);
+
+        /* TYPE: char
+           MIN:
+           MAX:
+           DEFAULT
+         */
+        char myCharVariable1 = 'a';
+        System.out.println("Char variable: " + myCharVariable1);
+        char myCharVariable2 = '&';
+        System.out.println("Char variable: " + myCharVariable2);
+        char myCharVariable3 = 93;
+        System.out.println("Char variable: " + myCharVariable3);
+
+    }
+}
---
+++ src/chapter_03/VariablesDefaultValues.java
@@ -0,0 +1,41 @@
+package Chapter03;
+
+public class VariablesDefaultValues {
+    static byte myByteVariable;
+
+    static short myShortVariable;
+
+    static int myIntVariable;
+
+    static long myLongVariable;
+
+    static float myFloatVariable;
+
+    static double myDoubleVariable;
+
+    static boolean myBooleanVariable;
+
+    static char myCharVariable;
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Default value for byte variable: " + myByteVariable);
+
+        System.out.println("Default value for short variable: " + myShortVariable);
+
+        System.out.println("Default value for int variable: " + myIntVariable);
+
+        System.out.println("Default value for long variable: " + myLongVariable);
+
+        System.out.println("Default value for float variable: " + myFloatVariable);
+
+        System.out.println("Default value for double variable: " + myDoubleVariable);
+
+        System.out.println("Default value for boolean variable: " + myBooleanVariable);
+
+        System.out.println("Default value for char variable: " + myCharVariable);
+
+
+    }
+}
