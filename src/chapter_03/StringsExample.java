b5b5ad882fc284e4cf79e5b0509f545143aa80e2
Neil.Crerar <Neil.Crerar@bjss.com>
2019-07-29 12:40:04 +0100

    further updates to string methods files

--- src/chapter_03/StringsExample.java
+++ src/chapter_03/StringsExample.java
@@ -6,4 +6,21 @@ and how this affects how it is stored and managed in memory.
 */
 
 public class StringsExample {
+
+    public static void main(String[] args) {
+        String myStringVariable = "This is a test string";
+        System.out.println("String as a string literal: " + myStringVariable);
+
+        String myStringVariableB = "This is a test string";
+        System.out.println("Second string literal with same contents: " + myStringVariableB);
+
+        myStringVariable = "Hey hey we're the monkees";
+        System.out.println("Updated only the first string literal: " + myStringVariable);
+        System.out.println("Second string literal with same contents: " + myStringVariableB);
+
+        String anotherStringVariable = new String("A String created as an object");
+        System.out.println("String as an object: " + anotherStringVariable);
+
+    }
+
 }
